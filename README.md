f9a36d266da7b6361079ec9f43e64581dbc03ba5
fabricio-p <fabriciopashaj.dev@gmail.com>
2025-01-25 16:46:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+physics-lab
+===========
+
+This is the typst source of my physics lab submission and the racket code that solves all the exercises.
+
+## TODO
+- [ ] Extract the diagram drawing functions into a library
---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "physics-lab")
+(define deps '("base" "plot" "metapict"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/physics-lab.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(fabricio))
+(define license '(Apache-2.0 OR MIT))
